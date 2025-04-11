9fe2750424019db2dbb9940855767ee75e550a8f
i.petrov <ipb.tech@yandex.ru>
2025-04-12 00:07:45 +0300

    tests

---
+++ EcoTrails.Tests/Client/Features/Home/GetTrailsHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using AutoFixture;
+using EcoTrails.Shared.Features.Home.Shared;
+using MediatR;
+
+namespace EcoTrails.Tests.Client.Features.Home;
+
+    public class GetTrailsHandler : IRequestHandler<GetTrailsRequest, GetTrailsRequest.Response>
+    {
+        public async Task<GetTrailsRequest.Response> Handle(GetTrailsRequest request, CancellationToken cancellationToken)
+        {
+            var fixture = new Fixture();
+            var dummyTrails = fixture.CreateMany<GetTrailsRequest.Trail>();
+
+            return new GetTrailsRequest.Response(dummyTrails);
+        }
+    }
